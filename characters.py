0a4fe6bdff20f2e0b5f088d947896390d41bde87
Shlok <shlokchoudhary22072007@gmail.com>
2025-12-27 00:16:55 +0530

    characters class ready with multiple characters incnluded with working methods and fighting logic

--- characters.py
+++ characters.py
@@ -36,20 +36,44 @@ class Knight(Character):
 
 
 class Goblin(Character):
-    def __init__(self):
-        super().__init__("Goblin", attack=5, hp=30, defense=5)
+    def __init__(self, name = "Goblin"):
+        super().__init__(name, attack=5, hp=30, defense=5)
 
     def special_power(self, target):
         dmg = target.take_damage(self.attack)
         self.hp = min(self.max_hp, self.hp + 5)
         print(f"Goblin uses STEAL for {dmg} damage and heals to {self.hp}/{self.max_hp} HP!")
 
+class Golem(Character):
+    def __init__(self,name):
+        super().__init__(name, attack = 5, hp = 150 ,defense = 20)
 
-if __name__ == "__main__":
-    hero = Knight()
-    villain = Goblin()
+    def special_power(self, target):
+        dmg = target.take_damage(self.attack)
+        self.defense += 5
+        print(f"The GOLEM uses special power to increase its defense to {self.defense} and inflicts an attack of {dmg}")
 
-    hero.attack_target(villain)
-    villain.special_power(hero)
+class Mage(Character):
+    def __init__(self, name = "Mage"):
+        super().__init__(name, attack = 10, hp = 50, defense = 10)
+        self.mana = 20
+        self.mana_max = 20
+
+    def special_power(self, target):
+        if self.mana < self.mana_max:
+            print("not enough mana, wait for it to recharge")
+            return 
+
+        dmg = target.take_damage(3*self.attack)
+        self.mana = 0
+        print(f"{self.name} casts a ligtening blast on {target.name} and inflicts {dmg} damage")
+        print(f"mana left = {self.mana}/{self.mana_max}")
+
+    def recharge_mana(self):
+        self.mana = min(self.mana_max, self.mana + 5)
+        print(f"mana = {self.mana}/{self.mana_max}")
+    
+        if (self.mana == self.mana_max):
+            print("Mana has been restored, special attack can be used now!")
 
 
