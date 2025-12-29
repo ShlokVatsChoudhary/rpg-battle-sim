d5d86d6d275179f79d7ae67a57ed97e641835f7a
Shlok <shlokchoudhary22072007@gmail.com>
2025-12-30 00:00:30 +0530

    added player's chance logic in battle class in battle_logic

--- battle_logic.py
+++ battle_logic.py
@@ -26,7 +26,7 @@ class Battle:
         if hasattr(character, "mana") and hasattr(character, "mana_max"):
             character.mana = min(character.mana_max, character.mana + amount)
 
-    def special_power(self,character): # permission to use mana based power
+    def can_use_special(self,character): # permission to use mana based power
         """Mage-style rule: special only when mana is FULL. Others can always special."""
         if hasattr(character, "mana") and hasattr(character, "mana_max"):
             return (character.mana == character.mana_max)
@@ -40,5 +40,43 @@ class Battle:
             else:
                 self.gain_mana(character,5)
 
+
+
+    def player_chance(self):
+        while (True):
+            print("CHOOSE YOUR ACTION: ",
+                  "1. Normal attack",
+                  "2. Special ability",
+                  "3. Skip turn")
+            
+            choice = input("Enter 1/2/3: ")
+
+            if choice not in ("1", "2", "3"):
+                print("ERROR!\nSelect from options 1/2/3!\n")
+                continue
+
+            else:
+                match (choice):
+                    case 1:
+                        self.player.attack_target(self.enemy)
+                        self.player.regeneration_special(self.player, skipped_turn = False)
+
+                    case 2:
+                        if not self.player.can_use_special(self.player):
+                            print("Not enough mana to perform special ability")
+                            continue
+                        else:
+                            self.player.special_power(self.enemy)
+                            self.player.regeneration_special(self.player, skipped_turn = False)
+
+                    case 3:
+                        print(f"{self.player} has skipped the turn for +15 MANA...")
+                        self.player.regeneration_special(self.player, skipped_turn = True)
+
+
+    # Enemy's turn (simple probabilistic AI)
+
+    def enemy_turn(self, enemy):
+        
 
 
